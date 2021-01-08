f9a6cbc3cba2dba69bc97de18b2f6c84c251bc63
tangc8 <tangc8@bngrp.com>
2021-01-08 16:33:22 +0800

    develop

--- conftest.py
+++ conftest.py
@@ -10,4 +10,5 @@ def initialize_tests(request):
     db_url = os.environ.get("TORTOISE_TEST_DB", "sqlite://:memory:")
     initializer(["tests.testmodels"], db_url=db_url)
     print('dfads')
+    print('develop')
     request.addfinalizer(finalizer)
