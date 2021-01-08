365655f28713e5d7ddecd3e0514233e3368d3789
tangc8 <tangc8@bngrp.com>
2021-01-08 16:49:52 +0800

    test

--- conftest.py
+++ conftest.py
@@ -9,6 +9,6 @@ from tortoise.contrib.test import finalizer, initializer
 def initialize_tests(request):
     db_url = os.environ.get("TORTOISE_TEST_DB", "sqlite://:memory:")
     initializer(["tests.testmodels"], db_url=db_url)
-    print('dfads')
+    print('dfadsdsfsdfs')
     print('develop34')
     request.addfinalizer(finalizer)
